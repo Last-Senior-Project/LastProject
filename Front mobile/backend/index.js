223d9a540fa2ac74db253d5746138b136f6993fe
BarBoochiTN <mouhamedalibarbouchi@gmail.com>
2023-05-02 10:36:45 +0100

    alo

--- Front mobile/backend/index.js
+++
@@ -1,7 +0,0 @@
-const app = require ('./App')
-const http = require('http').createServer(app)
-const port =5000;
-const db =require("./database/index")
-http.listen(port,()=>{
-    console.log(`Server listening on port ${port}`);
-})
